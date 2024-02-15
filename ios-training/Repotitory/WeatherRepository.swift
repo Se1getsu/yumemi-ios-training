1a97ee4b7e62ebb398caca225a62cc48c885a89d
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2024-02-15 11:07:34 +0900

    refactor: RepositoryErrorをWeatherRepository.APIErrorにリネーム

--- ios-training/Repotitory/WeatherRepository.swift
+++ ios-training/Repotitory/WeatherRepository.swift
@@ -10,11 +10,20 @@ import YumemiWeather
 
 /// YumemiWeather から天気を取得するリポジトリ
 struct WeatherRepository {
+    // MARK: Errors
+    
+    enum APIError: Error {
+        /// `Weather`に未定義の天気を取得した場合のエラー
+        case undefinedWeather
+    }
+    
+    // MARK: Internal
+    
     /// 天気を取得する
     func fetch(at area: String) throws -> Weather {
         let weatherString = try YumemiWeather.fetchWeatherCondition(at: area)
         guard let weather = Weather(rawValue: weatherString) else {
-            throw RepositoryError.undefinedWeather
+            throw APIError.undefinedWeather
         }
         return weather
     }
