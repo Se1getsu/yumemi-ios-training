152eb197df9b83744b59496ce9dee2ca7eebfd06
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2024-02-16 14:09:35 +0900

    feat: WeatherViewに遷移するホームビューを作成

---
+++ ios-training/HomeView/HomeViewController.swift
@@ -0,0 +1,41 @@
+//
+//  HomeViewController.swift
+//  ios-training
+//
+//  Created by 垣本 桃弥 on 2024/02/15.
+//
+
+import UIKit
+
+/// 起動時に表示されるホーム画面
+class HomeViewController: UIViewController {
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        transitToWeatherView()
+    }
+}
+
+// MARK: - Private
+
+private extension HomeViewController {
+    func transitToWeatherView() {
+        let vc = WeatherViewController(
+            weatherInfoRepository: WeatherInfoRepository()
+        )
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true)
+    }
+}
+
+// MARK: - Preview
+
+#Preview {
+    return HomeViewController()
+}
--- ios-training/SceneDelegate.swift
+++ ios-training/SceneDelegate.swift
@@ -14,9 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = WeatherViewController(
-            weatherInfoRepository: WeatherInfoRepository()
-        )
+        window?.rootViewController = HomeViewController()
         window?.makeKeyAndVisible()
     }
 
